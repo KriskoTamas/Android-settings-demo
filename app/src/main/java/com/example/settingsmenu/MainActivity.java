1953e2b1bafb7ad9e329818e4ff32e1b156deaa9
tomik <tomi.krisko@gmail.com>
2019-06-20 13:14:29 +0200

    Added main functionality

--- app/src/main/java/com/example/settingsmenu/MainActivity.java
+++ app/src/main/java/com/example/settingsmenu/MainActivity.java
@@ -5,33 +5,43 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
+// import android.support.v7.app.AppCompatActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.preference.Preference;
 // import android.support.v7.app.AppCompatActivity;
-import androidx.preference.Preference.OnPreferenceClickListener;
-import androidx.preference.PreferenceCategory;
 
 public class MainActivity extends AppCompatActivity implements SharedPreferences.OnSharedPreferenceChangeListener {
 
-    TextView visibletxt, colortxt, sizetxt;
+    TextView customText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        visibletxt = findViewById(R.id.visible_text);
-        colortxt = findViewById(R.id.color_text);
-        sizetxt = findViewById(R.id.size_text);
+        customText = findViewById(R.id.customText);
+
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        customText.setVisibility(prefs.getBoolean("display", true) ? View.VISIBLE : View.INVISIBLE);
+        customText.setTextSize(Float.parseFloat(prefs.getString("size", "15")));
+        String pref_color_value = prefs.getString("color", "black");
+        try{
+            customText.setTextColor(Color.parseColor(pref_color_value));
+        }
+        catch(Exception e){
+            customText.setTextColor(Color.parseColor("black")); // you can use hex (#ff0000) color or color names like "red"
+        }
+        // Toast.makeText(this, "wow!", Toast.LENGTH_LONG).show();
 
         setupSharedPreferences();
     }
@@ -42,12 +52,35 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     // Method to set Visibility of Text.
-    private void setTextVisible(boolean display_text) {
-        if (display_text == true) {
-            visibletxt.setVisibility(View.VISIBLE);
-        } else {
-            visibletxt.setVisibility(View.INVISIBLE);
+    private void setTextVisible(boolean display) {
+        customText.setVisibility(display ? View.VISIBLE : View.INVISIBLE);
+    }
+
+    // Method to set Color of Text.
+    private void changeTextColor(String pref_color_value) {
+        Log.d("Parzival", pref_color_value);
+        try{
+            customText.setTextColor(Color.parseColor(pref_color_value));
         }
+        catch(Exception e){
+            customText.setTextColor(Color.parseColor("black")); // you can use hex (#ff0000) color or color names like "red"
+        }
+    }
+
+    // Method to set Size of Text.
+    private void changeTextSize(Float i) {
+        customText.setTextSize(i);
+    }
+
+    // Method to pass value from SharedPreferences
+    private void loadColorFromPreference(SharedPreferences sharedPreferences) {
+        Log.d("Parzival",sharedPreferences.getString(getString(R.string.pref_color_key),getString(R.string.pref_color_red_value)));
+        changeTextColor(sharedPreferences.getString(getString(R.string.pref_color_key),getString(R.string.pref_color_red_value)));
+    }
+
+    private void loadSizeFromPreference(SharedPreferences sharedPreferences) {
+        float minSize = Float.parseFloat(sharedPreferences.getString(getString(R.string.pref_size_key), "16.0"));
+        changeTextSize(minSize);
     }
 
     @Override
@@ -58,7 +91,7 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) { // detect click on option in action bar
+    public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
@@ -68,17 +101,15 @@ public class MainActivity extends AppCompatActivity implements SharedPreferences
         return super.onOptionsItemSelected(item);
     }
 
-    /*
-    if(key.equals("text_color")){
-            Toast.makeText(MainActivity.this,"color",Toast.LENGTH_LONG).show();
-        }
-    */
-
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) { // changing the state of an option (not click)
-        if (key.equals("display_text")) {
-            setTextVisible(sharedPreferences.getBoolean("display_text", true));
-            // openDialog("Most épp egy nagyon fontos döntés előtt állsz. Igen vagy nem?");
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+
+        if (key.equals("display")) {
+            setTextVisible(sharedPreferences.getBoolean("display",true));
+        } else if (key.equals("color")) {
+            loadColorFromPreference(sharedPreferences);
+        } else if (key.equals("size"))  {
+            loadSizeFromPreference(sharedPreferences);
         }
     }
 
